e5f86deb538e1dc3647f291a502df8e1135bc261
0xTop10 <wangchen@wangchendeMacBook-Pro.local>
2025-05-19 19:45:14 +0800

    change bot

--- backend/src/bot.js
+++ backend/src/bot.js
@@ -3,108 +3,150 @@ import { getIO } from './websocket/socketHandler.js';
 import { redisService } from './utils/redisHandler.js';
 
 
-export const runBot = async function main() {
+export const addBotToRoom = async () => {
     const prisma = new PrismaClient();
-    //判断游戏是否在wating状态
-    const game = await prisma.game.findFirst({
-        where: {
-            status: {
-                not: "done"
-            }
-        },
-    });
-    if (!game) {
-        return;
-    }
-    const gameId = game.id;
-
-    const gameCount = await prisma.game_play.count({
-        where: {
-            game_id: gameId,
-            bet_coin: { gt: 0 }
+    try {
+        //判断游戏是否在wating状态
+        const game = await prisma.game.findFirst({
+            where: {
+                status: {
+                    not: "done"
+                }
+            },
+        });
+        if (!game) {
+            return;
         }
-    });
-    if (gameCount >= 20) {
-        return;
-    }
-    //确保每个房间都要有用户bet  如果没有  就加入机器人
-    const gamePlay = await prisma.game_play.groupBy({
-        by: ['room_id'],
-        where: {
-            game_id: gameId,
-        },
-        _sum: {
-            bet_coin: true   // 计算每个组的bet总和
-        }
-    });
-    if (!gamePlay) {
-        return;
-    }
-    const allRoomId = [1, 2, 3, 4, 5, 6, 7, 8];
-    for (let i = 0; i < gamePlay.length; i++) {
-        const room_id = gamePlay[i].room_id;
-        const bet_coin = gamePlay[i]._sum.bet_coin;
-        if (bet_coin > 0) {
-            const index = allRoomId.indexOf(room_id);
-            if (index > -1) {
-                allRoomId.splice(index, 1);
-            }
-        }
-    }
-    //随机选一个房间增加机器人
-    const randomRoomId = allRoomId[Math.floor(Math.random() * allRoomId.length)];
-    const users = await prisma.user.findMany({
-        where: {
-            is_bot: 1
-        },
-        select: { id: true }
-    });
-    const userIds = users.map(user => user.id);
-    //随机选一个用户
-    const randomUserId = userIds[Math.floor(Math.random() * userIds.length)];
-    console.log("randomUserId", randomUserId);
-    const randomBet = Math.floor(Math.random() * 10) + 1;
-    //插入数据
-    const cacheKey = "user_id_to_address";
-    const userAddr = await redisService.hGet(cacheKey, `uid_${randomUserId}`);
-    //50%的概率加入机器人
-    const io = getIO();
-    if (Math.random() > 0.5) {
-        io.emit('user_joined_room', { userId: randomUserId, type: "user_joined_room", address: userAddr });
-        return;
-    }
-    //查询是否存在
-    const gamePlayInfo = await prisma.game_play.findFirst({
-        where: {
-            user_id: randomUserId,
-            game_id: gameId,
-        },
-        select: { id: true }
-   });
-    if (!gamePlayInfo) {
-        await prisma.game_play.create({
-            data: {
+        const gameId = game.id;
+        const gameCount = await prisma.game_play.count({
+            where: {
                 game_id: gameId,
-                user_id: randomUserId,
-                room_id: randomRoomId,
-                bet_coin: randomBet
+                bet_coin: { gt: 0 }
             }
         });
-        const result = await prisma.$queryRaw`
+        if (gameCount >= 20) {
+            return;
+        }
+        //确保每个房间都要有用户bet  如果没有  就加入机器人
+        const gamePlay = await prisma.game_play.groupBy({
+            by: ['room_id'],
+            where: {
+                game_id: gameId,
+            },
+            _sum: {
+                bet_coin: true   // 计算每个组的bet总和
+            }
+        });
+        if (!gamePlay) {
+            return;
+        }
+        const allRoomId = [1, 2, 3, 4, 5, 6, 7, 8];
+        for (let i = 0; i < gamePlay.length; i++) {
+            const room_id = gamePlay[i].room_id;
+            const bet_coin = gamePlay[i]._sum.bet_coin;
+            if (bet_coin > 0) {
+                const index = allRoomId.indexOf(room_id);
+                if (index > -1) {
+                    allRoomId.splice(index, 1);
+                }
+            }
+        }
+
+        if (allRoomId.length == 0) {
+            return;
+        }
+        const users = await prisma.user.findMany({
+            where: {
+                is_bot: 1
+            },
+            select: { id: true }
+        });
+        const userIds = users.map(user => user.id);
+        //随机选一个用户
+        const randomUserId = userIds[Math.floor(Math.random() * userIds.length)];
+        //随机选一个房间增加机器人
+        const randomRoomId = allRoomId[Math.floor(Math.random() * allRoomId.length)];
+        const randomBet = Math.floor(Math.random() * 10) + 1;
+        //插入数据
+        //查询是否存在
+        const gamePlayInfo = await prisma.game_play.findFirst({
+            where: {
+                user_id: randomUserId,
+                game_id: gameId,
+            },
+            select: { id: true }
+        });
+        if (!gamePlayInfo) {
+            console.log("roomId", randomRoomId);
+            await prisma.game_play.create({
+                data: {
+                    game_id: gameId,
+                    user_id: randomUserId,
+                    room_id: randomRoomId,
+                    bet_coin: randomBet
+                }
+            });
+            const result = await prisma.$queryRaw`
             SELECT SUM(bet_coin) as total
             FROM game_play
             WHERE game_id = ${gameId} AND room_id = ${randomRoomId}
         `;
-        const totalBetAmount = Number(result[0]?.total) || 0;
+            const totalBetAmount = Number(result[0]?.total) || 0;
+            const cacheKey = "user_id_to_address";
+            const userAddr = await redisService.hGet(cacheKey, `uid_${randomUserId}`);
+
+            const userInfo = {
+                user_id: randomUserId,
+                user_addr: userAddr
+            }
+            const io = getIO();
+            io.emit('room_add_user', { roomId: randomRoomId, type: "room_add_user", roomAmount: totalBetAmount, userId: randomUserId, userInfo });
+        }
+    } catch (err) {
+        console.log(err);
+    } finally {
+        await prisma.$disconnect();
+    }
+    return
+}
+
+export const addBotToWait = async () => {
+    const prisma = new PrismaClient();
+    //判断游戏是否在wating状态
+    try {
+        const game = await prisma.game.findFirst({
+            where: {
+                status: {
+                    not: "done"
+                }
+            },
+        });
+        if (!game) {
+            return;
+        }
+        const users = await prisma.user.findMany({
+            where: {
+                is_bot: 1
+            },
+            select: { id: true }
+        });
+        const userIds = users.map(user => user.id);
+        //随机选一个用户
+        const randomUserId = userIds[Math.floor(Math.random() * userIds.length)];
         const cacheKey = "user_id_to_address";
         const userAddr = await redisService.hGet(cacheKey, `uid_${randomUserId}`);
-
-        const userInfo = {
-            user_id: randomUserId,
-            user_addr: userAddr
+        if (!userAddr) {
+            console.log(`No address found for bot user ${randomUserId}`);
+            return;
         }
-        io.emit('room_add_user', { roomId: randomRoomId, type: "room_add_user", roomAmount: totalBetAmount, userId: randomUserId, userInfo });
+        const io = getIO();
+        io.emit('user_joined', { userId: randomUserId, type: "user_joined", address: userAddr });
+    } catch (err) {
+        console.log(err);
+    } finally {
+        await prisma.$disconnect();
     }
+    return;
 }
 
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ import authRoutes from './routes/auth.js';
 import roomRoutes from './routes/room.js';
 import gameRoutes from './routes/game.js';
 import cron from 'node-cron';
-import { runBot } from './bot.js';
+import { addBotToRoom, addBotToWait } from './bot.js';
 import { initUserInfo } from './user.js';
 
 const app = express();
@@ -26,7 +26,7 @@ initSocketServer(server);
 
 // 每秒执行的任务
 let isTaskRunning = false;
-// cron.schedule('*/4 * * * * *', async () => {
+// cron.schedule('*/5 * * * * *', async () => {
 //   if (isTaskRunning) {
 //     console.log('上次任务尚未完成，跳过本次执行');
 //     return;
@@ -36,14 +36,35 @@ let isTaskRunning = false;
 //   const startTime = Date.now();
 //   try {
 //     console.log('任务开始:', new Date().toLocaleTimeString());
-//     await runBot();
+//     await addBotToRoom();
 //   } catch (error) {
 //     console.error('任务执行失败:', error);
 //   } finally {
 //     isTaskRunning = false;
-//     console.log(`任务完成，耗时 ${Date.now() - startTime}ms`);
+//     console.log(`添加机器人到room，任务完成，耗时 ${Date.now() - startTime}ms`);
 //   }
 // });
+
+let isTaskRunningWait = false;
+cron.schedule('*/2 * * * * *', async () => {
+  if (isTaskRunningWait) {
+    console.log('上次任务尚未完成，跳过本次执行');
+    return;
+  }
+
+  isTaskRunningWait = true;
+  const startTime = Date.now();
+  try {
+    console.log('任务开始:', new Date().toLocaleTimeString());
+    await addBotToWait();
+  } catch (error) {
+    console.error('任务执行失败:', error);
+  } finally {
+    isTaskRunningWait = false;
+    console.log(`添加机器人到等待区，任务完成，耗时 ${Date.now() - startTime}ms`);
+  }
+});
+
 initUserInfo();
 
 const PORT = process.env.PORT || 3000;
